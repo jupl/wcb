f3118332beaea0b704a17d8eacd5ae71bcfe05ed
jupl <juan.placencia.512@gmail.com>
2018-11-22 16:11:54 -0600

    Spelling error

--- src/index.ts
+++ src/index.ts
@@ -321,7 +321,7 @@ function addHtml({environment, html, log}: InternalOptions) {
         },
         charset: 'UTF-8',
         viewport: [
-          'minumum-scale=1',
+          'minimum-scale=1',
           'initial-scale=1',
           'width=device-width',
           'shrink-to-fit=no',
